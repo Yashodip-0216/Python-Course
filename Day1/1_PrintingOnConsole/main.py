c7cd79bef88cfaac2c1ecf3055a164ccbde7435a
Yash <yash@Yashs-MacBook-Air.local>
2024-07-24 15:55:24 +0530

    First day completed

---
+++ Day1/1_PrintingOnConsole/main.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+print("A 'single quote' inside a double quote")
+
+print('A "double quote" inside a single quote')
+
+print("Alternatively you can just \"escape\" the quote")
+
+print("Hello" + " " + "Yash")
+
---
+++ Day1/3_Variables/main.py
@@ -0,0 +1,12 @@
+name = "Yash"
+print(name)
+
+name = "Deep"
+print(name)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+valid_user_name = "Variable"
+print(valid_user_name)
