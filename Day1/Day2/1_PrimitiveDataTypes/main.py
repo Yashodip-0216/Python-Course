3615af187769a0a16ea3e6e579b2bcf4d8a83a6d
Yash <yash@Yashs-MacBook-Air.local>
2024-07-25 15:25:03 +0530

    Day 2 complete

---
+++ Day1/Day2/1_PrimitiveDataTypes/main.py
@@ -0,0 +1,26 @@
+#Data Types
+
+#String
+
+print("Hello"[4])
+
+print("123" + "Hello World")
+
+print("123" + "345")
+
+#Integer
+
+print(124 + 456)
+
+print(123_456_765)
+
+#Float
+
+print(3.14421)
+print(int(3.144221))
+#Boolean
+
+print(True)
+
+print(False)
+
---
+++ Day1/Day2/2_Conversion/main.py
@@ -0,0 +1,13 @@
+num_char = len(input("What is your name? "))
+
+print("Your name has " + str(num_char) + " characters.")
+
+print(type(num_char))
+
+a = str(123)
+
+print(type(a))
+
+print(70 + float("143.212"))
+
+print(str(70) + str(100))
